c6a18c6523f8f2c05e68ebe65d4ab916cd96d49f
Leonardo Ascenzi <lascenzi@colgate.edu>
2024-01-09 15:50:28 -0500

    fleshed out photos page (not using Insta API yet)

---
+++ src/data/photos_pagedata.js
@@ -0,0 +1,52 @@
+export const imageData = [
+    // Row 1
+    {
+        imageURL: "https://scontent-atl3-2.cdninstagram.com/v/t51.29350-15/412415914_3601299833447626_5310746497691766095_n.heic?stp=dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=wh9B6fOPDvsAX8ISh5b&_nc_ht=scontent-atl3-2.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AfCWYqwDHjyEk4Aaru3A_adBeksU-lOzVkrf-DOAla8egQ&oe=65A1243B",
+        postLink: "https://www.instagram.com/p/C1I8EO1oqAR/?img_index=1",
+        alt: "Photo of Green Lake near Manlius, NY"
+    },
+    {
+        imageURL: "https://scontent-atl3-2.cdninstagram.com/v/t51.29350-15/412524973_1079462543197701_660883836372755280_n.heic?stp=dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=_tPsiA5ZS-EAX8fgKxK&_nc_oc=AQmqfaHN4U9mxFAuE2yUvalAWCWqqtmkk72zVEPfxDLvLtF_1kT0civrGiMKpip1KfY&_nc_ht=scontent-atl3-2.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AfA1CtvM-CvEX3Oe1FMzjmmuW0McraiJlCDC-BP-ZtIOLg&oe=65A33896",
+        postLink: "https://www.instagram.com/p/C1I6sq5Ifts/?img_index=1",
+        alt: "Ed Sheeran concert with purple lighting"
+    },
+    {
+        imageURL: "https://scontent-atl3-2.cdninstagram.com/v/t51.29350-15/387697906_347610367729557_4006290698939691222_n.heic?stp=dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=sRSKYGV_dE4AX8JQcTQ&_nc_ht=scontent-atl3-2.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AfAI3cffYvCSLGo-aEzmuLx_swOFNW0WuFZFK3KAW3GuNQ&oe=65A389A1",
+        postLink: "https://www.instagram.com/p/CySG3MAoK_4/?img_index=1",
+        alt: "Wright Brothers Memorial stands on a hill in NC"
+    },
+
+    // Row 2
+    {
+        imageURL: "https://scontent-atl3-1.cdninstagram.com/v/t51.29350-15/385655482_1064933724749663_3753749024947161098_n.heic?stp=dst-jpg&_nc_cat=108&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=-u1Oa0NQ16oAX_see_p&_nc_ht=scontent-atl3-1.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AfAgtRwcbnj0qrfz5fVcNz03pJu_smQH4uY2MP0OZuQGAg&oe=65A3965C",
+        postLink: "https://www.instagram.com/p/Cx9BLgwL8Q0/?img_index=1",
+        alt: "Picture of downtown Boston"
+    },
+    {
+        imageURL: "https://scontent-atl3-1.cdninstagram.com/v/t51.29350-15/385774384_696697738989381_5533386445992401608_n.heic?stp=dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=6Fk-WAAHO4wAX9eBPhs&_nc_ht=scontent-atl3-1.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AfDO5L4FIEsFzoVCARau486vLjuxNW2REnYGFuhI1d5ALw&oe=65A2D9F8",
+        postLink: "https://www.instagram.com/p/Cx8_L3ar75R/?img_index=1",
+        alt: "Caviar on sushi at Icca"
+    },
+    {
+        imageURL: "https://scontent-atl3-1.cdninstagram.com/v/t51.29350-15/370515161_3603576356636034_2482328542523694919_n.heic?stp=dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=l-SAmD4g4mIAX-K4mfb&_nc_ht=scontent-atl3-1.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AfA-qQijr5qumjf8Vut1eSmnlcjD88tOcmBZoCkV7LdNfg&oe=65A25629",
+        postLink: "https://www.instagram.com/p/Cwc8K4YSP8B/?img_index=1",
+        alt: "Snow in the desert of Tucson, AZ"
+    },
+
+    // Row 3
+    {
+        imageURL: "https://scontent-atl3-2.cdninstagram.com/v/t51.29350-15/371116783_5084656918325183_7148954229950733734_n.heic?stp=dst-jpg&_nc_cat=111&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=MhGBmV-erCcAX8cJTBk&_nc_ht=scontent-atl3-2.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AfD7jvA51FNWXhe2btQ_ILG5DAKnG72AzqL1sSJ1Vqk7xQ&oe=65A3029A",
+        postLink: "https://www.instagram.com/p/Cwc6IqLSqAv/?img_index=1",
+        alt: "Moonrise over Lake Mead"
+    },
+    {
+        imageURL: "https://scontent-atl3-1.cdninstagram.com/v/t51.29350-15/352549847_1738138169954159_1957207626858899638_n.webp?stp=dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=WLXkID3fuS4AX8sG9zT&_nc_ht=scontent-atl3-1.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AfDMbZQc7ArVFruTQ3va4ZFFzdvzxhwYwMFFQ8Gu-DiNRw&oe=65A2F51F",
+        postLink: "https://www.instagram.com/p/CtO-dI6rTk9/?img_index=1",
+        alt: "Glowing squares from Disney World drinks"
+    },
+    {
+        imageURL: "https://scontent-atl3-2.cdninstagram.com/v/t51.29350-15/333631047_3497247120510033_6985875660359932665_n.webp?stp=dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=EtDNW8ZvYzcAX_ZSEEJ&_nc_ht=scontent-atl3-2.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AfDWdoQZSg4E82had2t9MeQizvxYpLGNfRfuTNIXCnXd7g&oe=65A24AB0",
+        postLink: "https://www.instagram.com/p/CpO90x3sMhu/?img_index=1",
+        alt: "Reflection of Mt Baker in pool of water"
+    },
+];
