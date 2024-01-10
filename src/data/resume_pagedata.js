0db31779bade0e0a10c5ea316785dd88013370a3
Leonardo Ascenzi <lascenzi@colgate.edu>
2024-01-09 21:00:57 -0500

    built out resume, minor styling tweaks

---
+++ src/data/resume_pagedata.js
@@ -0,0 +1,33 @@
+export const educationData = [
+    {
+        title: "Colgate University",
+        subtitle: "B.A. Computer Science & Physics",
+        dates: "Aug 2016 to May 2020",
+        location: "Hamilton, NY",
+        description: ["Completed German Minor", "Received Award for Outstanding Contribution to the COSC Community (+$100)", "Presented paper at Spring 2018 Conference"],
+        image: "https://yt3.ggpht.com/ytc/AIf8zZTAcGuBs5LM-Cd-ZWXF4zzdKnakxaBzb47n50hmLw=s68-c-k-c0x00ffffff-no-rj",
+        imageLink: "https://colgate.edu",
+    },
+]
+
+
+export const experienceData = [
+    {
+        title: "Carimus",
+        subtitle: "Software Engineer",
+        dates: "March 2021 to Present",
+        location: "Raleigh, NC",
+        description: ["Raytheon Project", "Web Development", "App Development"],
+        image: "https://media.licdn.com/dms/image/C4D0BAQEq5Yw50hWvqA/company-logo_200_200/0/1635360340821/carimus_logo?e=2147483647&v=beta&t=Q5PjPilBwn6aW14IvT3TRH39_REOxVBAXQwx3CCtI-4",
+        imageLink: "https://www.carimus.com",
+    },
+    {
+        title: "Prometheus Group",
+        subtitle: "Support Developer",
+        dates: "Aug 2020 to March 2021",
+        location: "Raleigh, NC",
+        description: ["Fixed bugs", "Stared at wall"],
+        image: "https://www.prometheusgroup.com/hs-fs/hubfs/raw_assets/public/Prometheus_Group_January2023/images/webclip_webclip.png?width=1085&name=webclip_webclip.png",
+        imageLink: "https://prometheusgroup.com",
+    },
+]
