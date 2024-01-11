00b3e84b2f231606a11b6466ec9476a4e712fbab
Leonardo Ascenzi <lascenzi@colgate.edu>
2024-01-10 23:11:37 -0500

    updated basic blog cards with examples

--- src/data/projects_pagedata.js
+++ src/data/projects_pagedata.js
@@ -1,10 +1,26 @@
 export const workProjectsData = [
-    // Row 1
     {
         title: "Raytheon Project",
         description: "Creating Springboot API",
         image: "https://prd-sc102-cdn.rtx.com/-/media/rtx/news-images/2020/10/13/gtf-engine-making-history-now-and-in-the-future/gtf_worker_engine.png?w=1440&rev=aa3150621b2349a8afe782feb12b269f&hash=6B972002E9387B1C13F782F9CCD9160B",
         imageLink: "/projects/raytheon",
         tags: ["Java", "SpringBoot", "PDM"]
+    },
+    {
+        title: "Contract AI",
+        description: "Built out WP Site",
+        image: "/project-resources/contract-ai/contract-ai-project-cover.png",
+        imageLink: "/projects/contract-ai",
+        tags: ["Wordpress", "React", "Typescript"]
+    },
+]
+
+export const personalProjectsData = [
+    {
+        title: "Raider Rumble",
+        description: "ThreeJS Web Game - Colgate University",
+        image: "/project-resources/raider-rumble/raider-rumble-project-cover.png",
+        imageLink: "/projects/raider-rumble-game",
+        tags: ["ThreeJS", "JQuery"]
     },
 ]
