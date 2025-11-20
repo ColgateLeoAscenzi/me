b2bc4512aceb7ebf602fa21fbd26461588ec2269
Leo Ascenzi <leo.ascenzi@gmail.com>
2025-11-20 13:11:17 -0500

    fixed photo links

--- src/data/photos_pagedata.js
+++ src/data/photos_pagedata.js
@@ -12,41 +12,41 @@ export const imageData = [
     },
     {
         imageURL: "featured-photos/featured-photo-3.jpg",
-        postLink: "",
+        postLink: "https://www.instagram.com/p/DAFCkUJIRDb/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
         alt: "A flower close-up in Vancouver, Canada"
     },
 
     // Row 2
     {
         imageURL: "featured-photos/featured-photo-4.jpg",
-        postLink: "",
-        alt: "Me standing atop a rock in Arcadia National Park"
+        postLink: "https://www.instagram.com/p/C_10ajWIfCr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+        alt: "Me standing atop a rock in Olympic National Park"
     },
     {
         imageURL: "featured-photos/featured-photo-5.jpg",
-        postLink: "",
-        alt: ""
+        postLink: "https://www.instagram.com/p/C_lJQv6xf58/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+        alt: "A boat observes the sunset on Mt. Washington"
     },
     {
         imageURL: "featured-photos/featured-photo-6.jpg",
-        postLink: "",
-        alt: ""
+        postLink: "https://www.instagram.com/p/C_R0cQHNFg5/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+        alt: "The Colgate University Chapel lit up at dusk"
     },
 
     // Row 3
     {
         imageURL: "featured-photos/featured-photo-7.jpg",
-        postLink: "",
-        alt: ""
+        postLink: "https://www.instagram.com/p/C-8ma_XSC1R/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+        alt: "Pink and green drink at a Colorado cafe"
     },
     {
         imageURL: "featured-photos/featured-photo-8.jpg",
-        postLink: "",
-        alt: ""
+        postLink: "https://www.instagram.com/p/C-gk6xIPcsw/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+        alt: "A small hut faces Cancun, Mexico"
     },
     {
         imageURL: "featured-photos/featured-photo-9.jpg",
-        postLink: "",
-        alt: ""
+        postLink: "https://www.instagram.com/p/C-YMxe1PV5I/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+        alt: "A view of Austin, Texas from Mount Bonnell"
     },
 ];
--- src/pages/education/01_colgate_university.md
+++ src/pages/education/01_colgate_university.md
@@ -6,6 +6,6 @@ location: 'Hamilton, NY'
 image: 'https://yt3.ggpht.com/ytc/AIf8zZTAcGuBs5LM-Cd-ZWXF4zzdKnakxaBzb47n50hmLw=s68-c-k-c0x00ffffff-no-rj'
 imageLink: 'https://www.colgate.edu'
 ---
-- [ACM Group 2018 Conference Paper](https://dl.acm.org/citation.cfm?id=3154522forACMGroup‘18Conference)
+- [ACM Group 2018 Paper](https://dl.acm.org/citation.cfm?id=3154522forACMGroup‘18Conference)
 - German Minor, Study Abroad at University Freiburg
 - Received Award for Outstanding Contribution to the COSC Community (+$100)
