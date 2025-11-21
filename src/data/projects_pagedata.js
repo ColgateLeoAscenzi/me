da53dd4c1da31dd1d9e9fd80104cb9b56d16e99e
Leo Ascenzi <leo.ascenzi@gmail.com>
2025-11-20 20:06:24 -0500

    add more project links

--- src/data/projects_pagedata.js
+++ src/data/projects_pagedata.js
@@ -1,21 +1,42 @@
 export const workProjectsData = [
     {
         title: "Raytheon Project",
-        description: "Creating Springboot API",
+        description: "Customizing Windchill PDM",
         image: "https://prd-sc102-cdn.rtx.com/-/media/rtx/news-images/2020/10/13/gtf-engine-making-history-now-and-in-the-future/gtf_worker_engine.png?w=1440&rev=aa3150621b2349a8afe782feb12b269f&hash=6B972002E9387B1C13F782F9CCD9160B",
         imageLink: "/projects/raytheon",
         tags: ["Java", "SpringBoot", "PDM"]
     },
     {
         title: "Contract AI",
-        description: "Built out WP Site",
+        description: "Modern Website Redesign",
         image: "/project-resources/contract-ai/contract-ai-project-cover.png",
         imageLink: "/projects/contract-ai",
         tags: ["Wordpress", "React", "Typescript"]
     },
+    {
+        title: "Joulebug",
+        description: "Gamified ESG Application",
+        image: "/project-resources/contract-ai/contract-ai-project-cover.png",
+        imageLink: "/projects/joulebug",
+        tags: ["React", "React Native"]
+    },
+    {
+        title: "MiLuma",
+        description: "Puerto Rico Energy Dashboard",
+        image: "/project-resources/contract-ai/contract-ai-project-cover.png",
+        imageLink: "/projects/miluma",
+        tags: ["React", "Java", "SpringBoot"]
+    },
 ]
 
 export const personalProjectsData = [
+    {
+        title: "Clipcut",
+        description: "AI Powered Video Clip Finder",
+        image: "/project-resources/raider-rumble/raider-rumble-project-cover.png",
+        imageLink: "/projects/clipcut",
+        tags: ["Python", "Firebase", "Llama CPP"]
+    },
     {
         title: "Raider Rumble",
         description: "ThreeJS Web Game - Colgate University",
--- src/pages/resume/02_carimus.md
+++ src/pages/resume/02_carimus.md
@@ -6,19 +6,19 @@ location: 'Raleigh, NC'
 image: 'https://media.licdn.com/dms/image/C4D0BAQEq5Yw50hWvqA/company-logo_200_200/0/1635360340821/carimus_logo?e=2147483647&v=beta&t=Q5PjPilBwn6aW14IvT3TRH39_REOxVBAXQwx3CCtI-4'
 imageLink: 'https://www.carimus.com'
 ---
-*Raytheon Windchill Project*
+**[Raytheon Windchill Project](/projects/raytheon)**
 - Maintained UI, data and workflow features related to customer deliverable data packages
 - Collaborated with Windchill vendor to test and improve data replication tool
 
-*Contract AI* - contract simplification service https://www.contract-ai.com/
+**[Contract AI](/projects/contract-ai)** - contract simplification service https://www.contract-ai.com/
 - Developed TS/React components for headless wordpress implementation
 - Designed GraphQL queries to transform user data from CMS into full pages
 
-*Joulebug* - Large-scale gamified ESG application  https://enterprise.joulebug.com/
+**[Joulebug](/projects/joulebug)** - Large-scale gamified ESG application  https://enterprise.joulebug.com/
 - Developed features in React Native for Android/IOS mobile environments.
 - Created analytics web dashboard allowing admin users to track statistics and generate reports
 
-*MiLuma*  - Puerto Rico’s Energy Portal https://miluma.lumapr.com/login
+**[MiLuma](/projects/mi-luma)**  - Puerto Rico’s Energy Portal https://miluma.lumapr.com/login
 - Built email/sms Java Springboot microservices to support 2FA and enable communication between users and support representatives via forms
 - Created login/sign-up flow, integrating with account, auth services and legacy db
 - Implemented Formik templates for various customer use cases
